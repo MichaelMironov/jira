320f91caf3a8a73f8dad26eee7325419a003878b
Michael Mironov <nemiron94@gmail.com>
2024-07-10 14:08:29 +0300

    Add issue

--- src/test/kotlin/api/ApiTests.kt
+++ src/test/kotlin/api/ApiTests.kt
@@ -2,14 +2,13 @@ package api
 
 import com.codeborne.selenide.logevents.SelenideLogger.step
 import io.qameta.allure.Issue
-import java.security.MessageDigest
 import org.junit.jupiter.api.Test
 import java.lang.Thread.sleep
 
 class ApiTests {
 
     @Test
-    @Issue("CRC-1")
+    @Issue("AVTO-1")
     fun firstApiTest() {
         step("[STEP #1]") {
             sleep(5_000L)
@@ -17,12 +16,4 @@ class ApiTests {
         }
     }
 
-    @Test
-    @Issue("CRC-1")
-    fun secondApiTest() {
-        step("[STEP #1]") {
-            sleep(5_000L)
-            println("API TEST #2")
-        }
-    }
 }
